e937ae52bd486c2d2f72572217f8fc83484197bd
Juan Pablo Isaza M. <jisaza@vaovatravel.com>
2024-10-22 20:26:58 -0500

    initial project with drawer navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,26 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import "./gesture-handler";
+import "react-native-reanimated";
+import * as SplashScreen from "expo-splash-screen";
+import HomeScreen from "./(tabs)";
+import TabTwoScreen from "./(tabs)/explore";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+const Drawer = createDrawerNavigator();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -27,11 +34,19 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Drawer.Navigator screenOptions={{ headerTitle: "Mi aplicación" }}>
+        <Drawer.Screen
+          component={HomeScreen}
+          name="(tabs)/index"
+          options={{ title: "Inicio" }}
+        />
+        <Drawer.Screen
+          component={TabTwoScreen}
+          name="(tabs)/explore"
+          options={{ title: "Explorar" }}
+        />
+      </Drawer.Navigator>
     </ThemeProvider>
   );
 }
